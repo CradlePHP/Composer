3896edf6daeea1bdac56d9f45b3e9a3b7c5dce9c
Charles Zamora <charles.andacc@gmail.com>
2018-03-14 21:37:55 +0800

    initial commit

---
+++ src/Command.php
@@ -0,0 +1,224 @@
+<?php // -->
+/**
+ * This file is part of the Cradle PHP Library.
+ * (c) 2016-2018 Openovate Labs
+ *
+ * Copyright and license information can be found at LICENSE.txt
+ * distributed with this package.
+ */
+
+namespace Cradle\Composer;
+
+use Composer\Console\Application;
+use Symfony\Component\Console\Input\StringInput;
+
+/**
+ * Programmatically use Composer we
+ * only provide the basic functionalities
+ * here e.g require, install, remove, update.
+ *
+ * @vendor   Cradle
+ * @package  Composer
+ * @author   John Doe <john@doe.com>
+ * @standard PSR-4
+ */
+class Command 
+{
+    /**
+     * Composer home
+     * 
+     * @var string $home
+     */
+    protected $home = null;
+
+    /**
+     * Composer parameters
+     * 
+     * @var string $parameters
+     */
+    protected $parameters = [];
+
+    /**
+     * Set composer home
+     * 
+     * @param string $home
+     * @return $this
+     */
+    public function __construct($home = null)
+    {
+        // if home is set
+        if ($home) {
+            // set the home
+            $this->home = $home;
+        } else {
+            // figure out composer home
+            $this->home = __DIR__ . '/../vendor/bin/composer';
+        }
+
+        // put composer home to env
+        putenv(sprintf('COMPOSER_HOME=%s', $this->home));
+    }
+
+    /**
+     * Add a composer command parameter
+     * 
+     * @param string $key
+     * @param string $value
+     * @return $this
+     */
+    public function addParameter($key, $value)
+    {
+        // has --?
+        if (strpos($key, '--') === false) {
+            // prepend --
+            $key = '--' . $key;
+        }
+
+        // set parameter
+        $this->parameters[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set composer home
+     * 
+     * @param string $home
+     * @return $this
+     */
+    public function setComposerHome($home)
+    {
+        // set composer home
+        $this->home = $home;
+
+        return $this;
+    }
+
+    /**
+     * Runs composer install
+     * 
+     * @return void
+     */
+    public function install()
+    {
+        // execute the command
+        return $this->command('install', $this->parameters);
+    }
+
+    /**
+     * Runs composer remove
+     * 
+     * @param string|array $packages
+     * @return void
+     */
+    public function remove($packages = null)
+    {
+        // array of packages?
+        if (is_array($packages)) {
+            // join packages
+            $packages = implode(' ', $packages);
+        }
+
+        // build out the command
+        $command = trim(sprintf('remove %s', $packages));
+
+        // execute the command
+        return $this->command($command, $this->parameters);
+    }
+
+    /**
+     * Runs composer require
+     * 
+     * @param string|array $packages
+     * @return void
+     */
+    public function require($packages = null)
+    {
+        // array of packages?
+        if (is_array($packages)) {
+            // join packages
+            $packages = implode(' ', $packages);
+        }
+
+        // build out the command
+        $command = trim(sprintf('require %s', $packages));
+
+        // execute the command
+        return $this->command($command, $this->parameters);
+    }
+
+    /**
+     * Runs composer update
+     * 
+     * @param string|array $packages
+     * @return void
+     */
+    public function update($packages = null)
+    {
+        // array of packages?
+        if (is_array($packages)) {
+            // join packages
+            $packages = implode(' ', $packages);
+        }
+
+        // build out the command
+        $command = trim(sprintf('update %s', $packages));
+
+        // execute the command
+        return $this->command($command, $this->parameters);
+    }
+
+    /**
+     * Executes a composer command
+     * 
+     * @param string $command
+     * @param array $parameters
+     * @return void
+     */
+    public function command($command = null, $parameters = [])
+    {
+        // build out the command
+        $parameters = $this->buildParameters($parameters);
+
+        // set parameters to command
+        $command = sprintf('%s %s', $command, $parameters);
+
+        // set string input
+        $input = new StringInput($command);
+
+        // initialize composer app
+        $app = new Application();
+        // set auto exit
+        $app->setAutoExit(false);
+        // run the application
+        $app->run($input);
+    }
+
+    /**
+     * Build out command parameters
+     * 
+     * @param array $parameters
+     * @return $this
+     */
+    private function buildParameters($parameters = [])
+    {
+        // results
+        $results = [];
+
+        // iterate on each parameters
+        foreach($parameters as $key => $value) {
+            // if value is null
+            if (is_null($value)) {
+                // append to results
+                $results[] = $key;
+
+                continue;
+            }
+
+            // append to results
+            $results[] = sprintf('%s=%s', $key, $value);
+        }
+
+        return implode(' ', $results);
+    }
+}
---
+++ src/Packagist.php
@@ -0,0 +1,205 @@
+<?php // -->
+/**
+ * This file is part of the Cradle PHP Library.
+ * (c) 2016-2018 Openovate Labs
+ *
+ * Copyright and license information can be found at LICENSE.txt
+ * distributed with this package.
+ */
+
+namespace Cradle\Composer;
+
+use Cradle\Curl\CurlHandler;
+
+/**
+ * Packagist API Class
+ *
+ * @vendor   Cradle
+ * @package  Composer
+ * @author   John Doe <john@doe.com>
+ * @standard PSR-4
+ */
+class Packagist 
+{
+    /**
+     * @const string API_HOST
+     */
+    const API_HOST = 'https://packagist.org';
+
+    /**
+     * @const string PACKAGE_LIST
+     */
+    const PACKAGE_LIST = '/packages/list.json';
+
+    /**
+     * @const string PACKAGE_SEARCH
+     */
+    const PACKAGE_SEARCH = '/search.json';
+
+    /**
+     * @const string GET_PACKAGE_META
+     */
+    const GET_PACKAGE_META = '/p/%s/%s.json';
+
+    /**
+     * Request query
+     * 
+     * @var array $query
+     */
+    protected $query = [];
+
+    /**
+     * Dynamically set properties.
+     * 
+     * @param string $name
+     * @param *mixed $args
+     * @return $this
+     */
+    public function __call($name, $args)
+    {
+        //if method starts with set
+        if (strpos($name, 'set') === 0) {
+            //choose separator
+            $separator = '_';
+
+            //transform method to column name
+            $key = substr($name, 3);
+            $key = preg_replace("/([A-Z0-9])/", $separator."$1", $key);
+            $key = substr($key, strlen($separator));
+            $key = strtolower($key);
+
+            //if arg isn't set
+            if (!isset($args[0])) {
+                //default is null
+                $args[0] = null;
+            }
+
+            // if key is query
+            if ($key == 'query') {
+                // set to q
+                $key = 'q';
+            }
+
+            // set to property
+            $this->query[$key] = $args[0];
+        }
+
+        return $this;
+    }
+
+    /**
+     * List package names based on
+     * the given query, tags, type
+     * or vendor.
+     * 
+     * @return array
+     */
+    public function lists()
+    {
+        // create request url
+        $url = $this->buildRequest(
+            self::API_HOST, 
+            self::PACKAGE_LIST, 
+            $this->query
+        );
+
+        // send request
+        $response = CurlHandler::i()
+            ->setUrl($url)
+            ->setCustomRequest('GET')
+            ->getJsonResponse();
+
+        return $response;
+    }
+
+    /**
+     * Search for packages based on
+     * the given query, tags, type.
+     * 
+     * @return array
+     */
+    public function search($page = null, $range = null)
+    {
+        // if page is set
+        if ($page) {
+            // set page
+            $this->query['page'] = $page;
+        }
+
+        // if range is set
+        if ($range) {
+            // set range
+            $this->query['range'] = $range;
+        }
+
+        // create request url
+        $url = $this->buildRequest(
+            self::API_HOST, 
+            self::PACKAGE_SEARCH, 
+            $this->query
+        );
+
+        // send request
+        $response = CurlHandler::i()
+            ->setUrl($url)
+            ->setCustomRequest('GET')
+            ->getJsonResponse();
+
+        return $response;
+    }
+
+    /**
+     * Get package meta data.
+     * 
+     * @param string $vendor
+     * @param string $package
+     * @return array
+     */
+    public function getPackage($vendor, $package)
+    {
+        // create request url
+        $url = $this->buildRequest(
+            self::API_HOST, 
+            sprintf(
+                self::GET_PACKAGE_META,
+                $vendor,
+                $package
+            ),
+            $this->query
+        );
+
+        // send request
+        $response = CurlHandler::i()
+            ->setUrl($url)
+            ->setCustomRequest('GET')
+            ->getJsonResponse();
+
+        return $response;
+    }
+
+    /**
+     * Builds a request query based
+     * on the given url, path and query.
+     * 
+     * @param string $url
+     * @param string $path
+     * @param array $query
+     * @return $this
+     */
+    private function buildRequest($url, $path, $query = [])
+    {
+        // request url
+        $url = $url . $path;
+
+        // build query
+        $query = http_build_query($query);
+
+        // if we have query
+        if (!empty($query)) {
+            // set query
+            $url = $url . '?' . $query;
+        }
+
+        return $url;
+    }
+}
